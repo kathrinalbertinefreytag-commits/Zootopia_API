4265e77c2683254a359d0032cfa92c29739680db
“kathrinalbertinefreytag-commits” <kathrinalbertinefreytag@yahoo.com>
2025-12-12 19:26:00 +0100

    corrected File

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,12 +1,20 @@
-def fetch_data():
-"""fetching the animal data via API call"""
-URL = https://api.api-ninjas.com/v1/animals
+import requests
+
+API_URL = "https://api.api-ninjas.com/v1/animals"
+API_KEY = os.getenv("API_KEY")
+
+def fetch_data(query: str):
+    """Fetch animal data via API call from API Ninjas."""
     try:
         response = requests.get(
             API_URL,
-
             headers={"X-Api-Key": API_KEY},
             params={"name": query}
         )
         response.raise_for_status()
         animals_data = response.json()
+        return animals_data
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+        return None
